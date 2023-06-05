72af61559e6fac8972f5161b59a7c2095b7c82bf
Oscar Lechuga <olechugav1311@gmail.com>
2023-06-04 22:02:22 -0600

    routing updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
+import { CommingSoonComponent } from './components/commons/comming-soon/comming-soon.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
+  { path: 'proximamente', component: CommingSoonComponent },
 ];
 
 @NgModule({
