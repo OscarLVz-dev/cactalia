e4c2c65c6ee0843d0fdd5d33a0ed6ad462aa15e2
Oscar Lechuga <olechugav1311@gmail.com>
2023-06-19 18:08:28 -0600

    Space added

--- src/app/components/commons/loading-spinner/loading-spinner.component.ts
+++ src/app/components/commons/loading-spinner/loading-spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'loading-spinner',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./loading-spinner.component.scss']
 })
 export class LoadingSpinnerComponent {
+
+  @Input() loading=false;
 
 }
