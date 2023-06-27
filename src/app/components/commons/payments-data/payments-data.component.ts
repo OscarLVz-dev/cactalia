07be7fa25e5cca961b947995837676deee2a62a0
Oscar Lechuga <olechugav1311@gmail.com>
2023-06-27 10:53:01 -0600

    Payment data component ok

--- src/app/components/commons/payments-data/payments-data.component.ts
+++ src/app/components/commons/payments-data/payments-data.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Clipboard } from '@angular/cdk/clipboard';
+import iziToast from 'izitoast';
+import { PayData } from 'src/app/constants/PayData';
 
 @Component({
   selector: 'payments-data',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./payments-data.component.scss']
 })
 export class PaymentsDataComponent {
+
+  public PayDataEnum = PayData;
+
+  constructor(private clipboard: Clipboard) { }
+
+  /**
+   * Copy text to clipboard
+   * @param text text to copy
+   */
+  copyText(payData: PayData) {
+    if (this.clipboard.copy(payData.value)) {
+      iziToast.success({
+        title: '¡Exito!',
+        message: payData.displayName + ' copiada.',
+      });
+    } else {
+      iziToast.error({
+        title: 'Error',
+        message: 'Texto no copiado.',
+      });
+    }
+  }
 
 }
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { Clipboard } from '@angular/cdk/clipboard';
-import iziToast from 'izitoast';
-import { PayData } from 'src/app/constants/PayData';
 
 @Component({
   selector: 'main-component',
@@ -9,27 +6,5 @@ import { PayData } from 'src/app/constants/PayData';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
-
-  public PayDataEnum = PayData;
-
-  constructor(private clipboard: Clipboard) { }
-
-  /**
-   * Copy text to clipboard
-   * @param text text to copy
-   */
-  copyText(payData: PayData) {
-    if (this.clipboard.copy(payData.value)) {
-      iziToast.success({
-        title: '¡Exito!',
-        message: payData.displayName+' copiada.',
-      });
-    } else {
-      iziToast.error({
-        title: 'Error',
-        message: 'Texto no copiado.',
-      });
-    }
-  }
 
 }
