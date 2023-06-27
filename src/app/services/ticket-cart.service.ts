eaa15673c4290fddaeec37f82a42c0a392e32aa7
Oscar Lechuga <olechugav1311@gmail.com>
2023-06-26 23:26:11 -0600

    Deploy

--- src/app/services/ticket-cart.service.ts
+++ src/app/services/ticket-cart.service.ts
@@ -113,7 +113,7 @@ export class TicketCartService {
    * Send cart
    */
   sendCart() {
-    let headerText = "----------------------------------%0A⠀⠀⠀⠀⠀⠀⠀ *_CACTALIA_*🌵 %0A----------------------------------%0A⠀⠀ *MI CARRITO DE COMPRAS*%0A%0A*PRODUCTOS:*";
+    let headerText = "----------------------------------%0A⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ *_CACTALIA_*🌵 %0A----------------------------------%0A⠀⠀⠀⠀⠀⠀ *MI CARRITO DE COMPRAS*%0A%0A*PRODUCTOS:*";
     let products = "";
     let footer = "%0A%0A----------------------------------%0A *TOTAL:* $"+this.getTotal()+"%0A%0A ¡Hola! Te envio mi carrito de compras 🛍️😁.";
     
