9817539e92f149934e102ef7d5e178b22bfd0af6
OscarLVz <emmanuellv@hotmail.com>
2023-10-21 16:41:49 -0600

    catalog global added

---
+++ src/app/components/pages/catalog/catalog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'catalog-component',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.scss']
+})
+export class CatalogComponent {
+
+}
