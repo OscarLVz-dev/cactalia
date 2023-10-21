69c7fed12f2da6626f5127a9969d0fb9d9718249
OscarLVz <emmanuellv@hotmail.com>
2023-10-21 16:29:30 -0600

    env updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,13 @@
 export const environment = {
     production: false,
+
+    cactalia_phone:"",
+    cactalia_ticket_storage_name:"",
+
+    google_api_service_account:"",
+    google_api_key:"",
+
+    google_drive_folder_id_pines:"",
+    google_drive_folder_id_accesories:"",
+
 };
