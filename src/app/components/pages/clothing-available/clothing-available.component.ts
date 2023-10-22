f2e0307e0a0c4a3318b7104a9699cd9d69cd7506
OscarLVz <emmanuellv@hotmail.com>
2023-10-22 17:56:54 -0600

    component content

--- src/app/components/pages/clothing-available/clothing-available.component.ts
+++ src/app/components/pages/clothing-available/clothing-available.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { ProductType } from 'src/app/constants/ProductType';
+import { GoogleAuthServiceService } from 'src/app/services/google-auth-service.service';
+import { GoogleDriveServiceService } from 'src/app/services/google-drive-service.service';
+import { TicketCartService } from 'src/app/services/ticket-cart.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'clothing-available',
@@ -6,5 +11,78 @@ import { Component } from '@angular/core';
   styleUrls: ['./clothing-available.component.scss']
 })
 export class ClothingAvailableComponent {
+
+  public token;
+  public folders;
+  public folderSelected;
+  public folderSelectedQuantity: number = 0;
+  public folderSelectedPines = null;
+  public loading: boolean = false;
+  public ProductTypeEnum = ProductType;
+
+  constructor(
+    private ticketService: TicketCartService,
+    private googleDriveService: GoogleDriveServiceService,
+    private googleAuthService: GoogleAuthServiceService
+  ) {
+    this.loading = true;
+    this.googleAuthService.getBearerToken().subscribe(response => {
+      this.token = response.access_token;
+      this.googleDriveService.getFoldersInFolder(environment.google_drive_folder_id_pines, this.token).subscribe(response => {
+        this.folders = response.files;
+        this.loading = false;
+      }, error => {
+        this.loading = false;
+      });
+    }, error => {
+      this.loading = false;
+    });
+  }
+
+  /**
+   * Set folder selected
+   * @param folder 
+   */
+  selectFolder(folder: any) {
+    this.loading = true;
+    this.folderSelected = folder;
+    this.folderSelectedQuantity = 0;
+    this.googleDriveService.getFilesInFolder(folder.id, this.token).subscribe(response => {
+      if (response.files) {
+        response.files.forEach(file => {
+          if (file.description) {
+            let jsonData = JSON.parse(file.description);
+            file.name = jsonData.name;
+            file.desc = jsonData.desc;
+            file.price = jsonData.price;
+            file.quantity = jsonData.quantity;
+            if (file.quantity > 0) {
+              this.folderSelectedQuantity++;
+            }
+          }
+        });
+      }
+      this.folderSelectedPines = response.files;
+      this.loading = false;
+    }, error => {
+      this.loading = false;
+    });
+  }
+
+  /**
+   * Add item to cart
+   */
+  addItem(item) {
+    this.ticketService.addItem(
+      {
+        photo: item.webContentLink,
+        name: item.name,
+        description: item.desc,
+        category: this.ProductTypeEnum.pin.displayName,
+        price: item.price,
+        quantity: 1,
+      }
+    );
+  }
 
 }
