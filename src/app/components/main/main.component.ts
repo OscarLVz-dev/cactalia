64b37a81f2defdb6bb9890548e4c56e8cd41baf4
OscarLVz <emmanuellv@hotmail.com>
2023-12-06 20:24:37 -0600

    Alert on init

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'main-component',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+
+  ngOnInit(): void {
+    Swal.fire({
+      imageUrl: "https://drive.google.com/uc?id=16FZOinOImnOyX8ytLiDiQqxQ9J_mXPq5",
+      imageWidth: '100%',
+      backdrop: `#f7efecb5`,
+      showConfirmButton: false,
+      timer: 15000,
+      timerProgressBar: true,
+    });
+  }
 
 }
